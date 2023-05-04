d75df285788a184457a0a2f9a8616b973bb58aa9
Nancy <hernandezyanez@hotmail.com>
2023-05-04 10:26:24 -0400

    Basic view with IBOutlets and pods

--- MovieSearcherAlamofire/ViewController.swift
+++ MovieSearcherAlamofire/ViewController.swift
@@ -6,9 +6,24 @@
 //
 
 import UIKit
+import Alamofire
+import Kingfisher
+
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var movieName: UITextField!
+  
+  @IBOutlet weak var movieTitle: UILabel!
+  
+  @IBOutlet weak var launchDate: UILabel!
+  
+  @IBOutlet weak var awards: UILabel!
+  
+  @IBOutlet weak var actors: UILabel!
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
